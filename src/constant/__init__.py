76fd4ffdc18493bc81c588719550d968b742f0e2
CC-KEH <arbashhussain08@gmail.com>
2024-01-12 15:14:39 +0530

    MongoDB export and import

--- src/constant/__init__.py
+++
@@ -1,5 +0,0 @@
-from six.moves import urllib
-AWS_S3_BUCKET_NAME = 'sensor-deployment'
-MONGO_DATABASE_NAME  = 'sensor'
-MONGO_COLLECTION_NAME  = 'waferfault'
-MONGO_URI = "mongodb+srv://Arbi1234:"+urllib.parse.quote("Arbi1234")+"@sensor.iiinvmk.mongodb.net/?retryWrites=true&w=majority"
